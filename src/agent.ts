7767f1b673952dd907012167e87599612a842ab3
Kevin Yank <thatguy@kevinyank.com>
2025-06-25 17:59:51 +1000

    Implement the basic agent loop

--- src/agent.ts
+++ src/agent.ts
@@ -1,4 +1,5 @@
 import { AnthropicVertex } from "@anthropic-ai/vertex-sdk"
+import { Anthropic } from "@anthropic-ai/sdk"
 import { BadRequestError } from "@anthropic-ai/vertex-sdk/core/error.mjs"
 import * as readline from "readline/promises"
 
@@ -12,25 +13,57 @@ class Agent {
   ) {}
 
   async run() {
-    const userMessage = await this.getUserMessage()
-    try {
-      const result = await this.client.messages.create({
-        model: "claude-3-7-sonnet@20250219",
-        max_tokens: 100,
-        messages: [
-          {
-            role: "user",
-            content: userMessage,
-          },
-        ],
-      })
-      console.log(JSON.stringify(result, null, 2))
-    } catch (error) {
-      if (error instanceof BadRequestError) {
-        console.error("BadRequestError:", error.message)
-      } else {
-        console.error("Error:", error)
+    const conversation: Anthropic.MessageParam[] = []
+
+    console.log("Chat with Claude (use 'ctrl-c' to quit)")
+
+    while (true) {
+      const userMessage: Anthropic.MessageParam = {
+        role: "user",
+        content: await this.getUserMessage(),
       }
+      conversation.push(userMessage)
+
+      try {
+        const result: Anthropic.Message = await this.client.messages.create({
+          model: "claude-3-7-sonnet@20250219",
+          max_tokens: 1024,
+          messages: conversation,
+        })
+        conversation.push(this.messageToMessageParam(result))
+
+        for (const message of result.content) {
+          switch (message.type) {
+            case "text":
+              console.log(`\u001b[93mClaude\u001b[0m: ${message.text}`)
+          }
+        }
+      } catch (error) {
+        if (error instanceof BadRequestError) {
+          console.error("BadRequestError:", error.message)
+        } else {
+          console.error("Error:", error)
+        }
+      }
+    }
+  }
+
+  private messageToMessageParam(
+    message: Anthropic.Message,
+  ): Anthropic.MessageParam {
+    return {
+      role: message.role,
+      content: message.content
+        .map((c) => {
+          switch (c.type) {
+            case "text":
+              return c.text
+            default:
+              // Handle other content types if needed
+              return ""
+          }
+        })
+        .join(""),
     }
   }
 }
@@ -56,7 +89,7 @@ async function getUserMessage(): Promise<string> {
   })
 
   // Prompt user for input
-  const userMessage = await rl.question("Enter your message for Claude: ")
+  const userMessage = await rl.question("\u001b[94mYou\u001b[0m: ")
 
   // Close the readline interface
   rl.close()
